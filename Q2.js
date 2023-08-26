5207dd43b73b0975855c43367f4c701fdc95d9cb
noor92 <139437707+noor92@users.noreply.github.com>
2023-08-26 18:44:51 +0500

    Delete Q2.js

--- Q2.js
+++
@@ -1,9 +0,0 @@
-import inquirer from "inquirer";
-let answers = await inquirer.prompt([
-    {
-        name: "volume",
-        type: "number",
-        message: "Enter the volume of a cube:",
-    },
-]);
-console.log("The volume of the cube is:", answers.volume ** 3);
