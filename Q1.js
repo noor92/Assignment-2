9619aaa999eb90632c1d3e935b466d9992a2d527
noor92 <139437707+noor92@users.noreply.github.com>
2023-08-26 18:44:30 +0500

    Delete Q1.js

--- Q1.js
+++
@@ -1,7 +0,0 @@
-/*Exercise 1:  Write a program that calculates the area of a rectangle.*/
-console.log("Answer");
-function calculateRectangleArea(width, height) {
-    return (width * height);
-}
-console.log(calculateRectangleArea(5, 10));
-export {};
